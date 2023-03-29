1ebf422cd046d44444503e8bd7777c1eb81709ec
Bilkish-akther <bilkishakther921@gmail.com>
2023-03-29 14:46:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './component/Button/Button';
+import Header from "./component/Header/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Button>Sort By Date</Button>
+     
+      
+
+    </>
+  );
+};
+
+export default App;
---
+++ src/component/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+   
+    const Button = ({children}) => {
+
+    return (
+        <>
+            <button className="btn btn-accent">{children}</button>
+        </>
+    );
+};
+
+export default Button;
